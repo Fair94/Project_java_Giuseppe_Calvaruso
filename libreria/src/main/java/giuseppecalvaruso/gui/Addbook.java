5d95071e12b7ee082d8ee315ee2b66fac882f3b2
Fair94 <due.a.muzzo@gmail.com>
2025-08-28 15:52:49 +0200

    Finally final work:
    Trying to work on GUI

--- libreria/src/main/java/giuseppecalvaruso/gui/Addbook.java
+++ libreria/src/main/java/giuseppecalvaruso/gui/Addbook.java
@@ -2,10 +2,14 @@ package giuseppecalvaruso.gui;
 
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
+import giuseppecalvaruso.exception.Exception_Shield;
+import giuseppecalvaruso.exception.InputError;
 import giuseppecalvaruso.menu.Facade;
 
 public class Addbook extends JFrame{
@@ -15,7 +19,7 @@ public class Addbook extends JFrame{
         this.setSize(400,400);
         this.setLayout(new GridLayout(0,2,5,5));
 
-        JLabel titlelLabel = new JLabel("Title");
+        JLabel titleLabel = new JLabel("Title");
         JTextField titleField = new JTextField();
 
         JLabel ISBNLabel = new JLabel("ISBN");
@@ -24,7 +28,49 @@ public class Addbook extends JFrame{
         JLabel AuthorLabel = new JLabel("Author");
         JTextField AuthorField = new JTextField();
 
-        
+        JLabel PriceLabel = new JLabel("Price");
+        JTextField PriceField = new JTextField();
+
+        JLabel YearLabel = new JLabel("Publication Year");
+        JTextField YearField = new JTextField();
+
+        JLabel GenreLabel = new JLabel("Genre");
+        JTextField GenreField = new JTextField();
+
+        JButton ConfirmingButton = new JButton("Add Book");
+
+        this.add(titleLabel);
+        this.add(ISBNLabel);
+        this.add(AuthorLabel);
+        this.add(PriceLabel);
+        this.add(YearField);
+        this.add(GenreLabel);
+        this.add(ConfirmingButton);
+
+        ConfirmingButton.addActionListener(e->{
+            String Title = titleField.getText().trim();
+            String ISBN = ISBNLabel.getText().trim();
+            String Author = AuthorField.getText().trim();
+            String Price = PriceField.getText().trim();
+            String PublicationYear = YearField.getText().trim();
+            String Genre = GenreField.getText().trim();
+
+            String operation  = Exception_Shield.thePolice(()->{
+            if( Title.isEmpty()||
+                ISBN.isEmpty()||
+                Author.isEmpty()||
+                Price.isEmpty()||
+                PublicationYear.isEmpty()||
+                Genre.isEmpty()){
+                    throw new InputError("One or more fields are empty");
+                    }
+                
+                facade.AddingBook(Title,ISBN,Author,Price,PublicationYear,Genre);
+            }
+
+        });
+
+
 
 
         
